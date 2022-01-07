dd3833f61f19b3789057a275684fe720755bd8bd
bleo <bleo@mit.edu>
2022-01-07 17:02:38 -0500

    added music library

--- src/songBank.h
+++ src/songBank.h
@@ -1,34 +1,29 @@
-#include <../lib/Arduino-piezo-music/src/piezo-music.h>
+#include <./piezo-music.h>
 // ------------------ SONG BANK -------------------------
 int susMogiusgtusBPM = 30;
 int susMogiusgtusMelody[] = {
-  C4, DS4, F4, FS4, 
-  F4, DS4, C4, PAUSE,
-  AS3, D4, C4,
-  PAUSE,
-  G1, C2
-};
+    C4, DS4, F4, FS4,
+    F4, DS4, C4, PAUSE,
+    AS3, D4, C4,
+    PAUSE,
+    G1, C2};
 int rereSusMogiusgtusMelody[] = {
-  C4, DS4, F4, FS4, 
-  E4, DS4, C4, PAUSE,
-  AS3, B4, C4,
-  PAUSE,
-  G1, C2
-};
+    C4, DS4, F4, FS4,
+    E4, DS4, C4, PAUSE,
+    AS3, B4, C4,
+    PAUSE,
+    G1, C2};
 int susMogiusgtusRhythm[] = {
-  8, 8, 8, 8, 
-  8, 8, 4, 8,
-  16, 16, 4,
-  8,
-  8, 4
-};
+    8, 8, 8, 8,
+    8, 8, 4, 8,
+    16, 16, 4,
+    8,
+    8, 4};
 
 int stereoLoveBPM = 60;
 int stereoLoveMelody[] = {
-  GS4, PAUSE, E5, PAUSE, E5, DS5, E5, FS5, E5,
-  DS5, E5, CS5, DS5, B4, CS5, A4, B4, GS4
-};
+    GS4, PAUSE, E5, PAUSE, E5, DS5, E5, FS5, E5,
+    DS5, E5, CS5, DS5, B4, CS5, A4, B4, GS4};
 int stereoLoveRythm[] = {
-  8, 16, 6, 16, 8, 8, 8, 8, 8,
-  8, 8, 8, 8, 8, 8, 8, 8, 2
-};
+    8, 16, 6, 16, 8, 8, 8, 8, 8,
+    8, 8, 8, 8, 8, 8, 8, 8, 2};
